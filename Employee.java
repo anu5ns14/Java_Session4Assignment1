7d8eae07bf5167e266fd76d9dcb460fc317c6b63
Anurag Choudhary <anu5ns14@gmail.com>
2016-09-10 17:59:53 +0530

    Employee Class
    Encapsulated Employee Class

---
+++ Employee.java
@@ -0,0 +1,30 @@
+package Com.Training.Oops;
+
+public class Employee {
+		
+	private	int eid = 01;
+	private	String ename = "Anurag";
+	private	double esal = 50000.00;
+	
+	
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public double getEsal() {
+		return esal;
+	}
+	public void setEsal(double esal) {
+		this.esal = esal;
+	}
+		
+		
+}	
